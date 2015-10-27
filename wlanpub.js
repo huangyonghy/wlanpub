829b69297da04a6b43474e6928469371afd7941d
root <root@wlan-cloudserver1.(none)>
2015-10-27 08:56:28 +0800

    cas auth

--- wlanpub.js
+++ wlanpub.js
@@ -7,10 +7,12 @@
 
 var basic  = require('./lib/basic'),
     mqoper = require('./lib/mqoper'),
-    dboper = require('./lib/dboper');
+    dboper = require('./lib/dboper'),
+    casauth = require('./lib/cas-authentication');
 
 module.exports = {
-    "basic" : basic,
-    "mqhd"  : mqoper,
-    "dbhd"  : dboper
+    "basic"   : basic,
+    "mqhd"    : mqoper,
+    "dbhd"    : dboper,
+    "casauth" : casauth
 }
